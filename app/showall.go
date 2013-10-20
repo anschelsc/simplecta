eb46302a9a55b0630e06f80a9372b42127ce220d
Ye Paing <lazyye7@gmail.com>
2013-10-20 02:23:00 -0400

    Quick fix on Simplecta quering too many .rss posts from datasource.
    API Error 5. datasource_v3: timeout

--- app/showall.go
+++ app/showall.go
@@ -43,7 +43,7 @@ const showRaw = `
 	});
 </script>
 <head>
-<title>Simplecta!</title>
+<title>Complexcta!</title>
 <link rel="stylesheet" href="/static/main.css">
 </head>
 <body>
@@ -84,7 +84,12 @@ func showAll(w http.ResponseWriter, r *http.Request) {
 	}
 	infos := make([]*itemInfo, 0, ic)
 	iter := q.Run(c)
-	for {
+	count := 0			//Counter to prevent iter from requesting too many datasource request.
+	for {				
+		count++
+		if count >=101 { //I've limited the count to the first 100 post that it can find.
+			break		 //Will keep looking into alternatives for handling massive amounts of rss feeds.
+		}
 		sk, err := iter.Next(empty)
 		if err == datastore.Done {
 			break
