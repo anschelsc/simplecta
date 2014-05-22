f11ec04c4fca4143d73598661fb28f9c68bc9b13
Anschel Schaffer-Cohen <anschelsc@gmail.com>
2014-05-22 10:35:13 -0400

    Reconnect channel on window focus.

--- static/showall.js
+++ static/showall.js
@@ -1,9 +1,12 @@
 $(function() {
+	var connected = false;
 	var client = $("head").data("client");
 	var setup = function() {
 		$.getJSON("/getToken/", {client: client}, function(token) {
+			console.log("connecting");
 			var chan = new goog.appengine.Channel(token);
 			var sock = chan.open();
+			sock.onopen = function() { connected = true; };
 			sock.onmessage = function(m) {
 				var res = $.parseJSON(m.data);
 				var button = $("button[data-index="+res.Ind+"]");
@@ -16,10 +19,15 @@ $(function() {
 				button.text("mark " + mark);
 				button.data("mark", mark);
 			};
-			//sock.onclose = setup;
+			sock.onclose = function() { connected = false; };
 		});
 	};
 	setup();
+	window.onfocus = function() {
+		if (!connected) {
+			setup();
+		}
+	};
 
 	$(".ajax_link").click(function() {
 		var button = $(this);
